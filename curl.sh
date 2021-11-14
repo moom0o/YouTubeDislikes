a67fbb9b1ddfe3728f48db7f8a3a2cf877a73daa
moom0o <48740106+moom0o@users.noreply.github.com>
2021-11-14 18:59:05 -0500

    Fix the positional variables finally

--- curl.sh
+++ curl.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 while :; do
-echo "Starting download $1"
-curl -o "./videos/$1.json" --location --request POST 'https://www.youtube.com/youtubei/v1/next?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8' --header 'Content-Type: application/json' --data-raw '{
+echo "Starting download $1";
+curl -o "./videos/$1.json" --location --request POST 'https://www.youtube.com/youtubei/v1/next?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8' --header 'Content-Type: application/json' --data '{
     "context": {
         "client": {
             "hl": "en",
@@ -11,11 +10,11 @@ curl -o "./videos/$1.json" --location --request POST 'https://www.youtube.com/yo
             "clientVersion": "2.20210101"
         }
     },
-    "videoId": "$1"
-}' && if grep -q "dislike this video along with" "./videos/$1.json";then # Check if the json has dislikes, there's about a 10% chance of not being able to see dislikes, the program should simply restart the download.
-        echo "the string exists! $1";
+    "videoId": "'$1'"
+}' && if grep -q "dislike this video along with" "./videos/$1.json";then
+        echo "the string exists";
         break;
 else
-        echo "the string does not exist! $1"
+        echo "the string does not exist"
 fi
 done

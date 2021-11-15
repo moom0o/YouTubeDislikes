1d3931207a86a2387c7fefe3c458e072784d14e6
moom0o <48740106+moom0o@users.noreply.github.com>
2021-11-14 19:06:45 -0500

    prevent constant redownload of deleted video

--- curl.sh
+++ curl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COUNT=0
 while :; do
 echo "Starting download $1";
 curl -o "./videos/$1.json" --location --request POST 'https://www.youtube.com/youtubei/v1/next?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8' --header 'Content-Type: application/json' --data '{
@@ -16,5 +17,11 @@ curl -o "./videos/$1.json" --location --request POST 'https://www.youtube.com/yo
         break;
 else
         echo "the string does not exist"
+        let COUNT++
+        if [ $COUNT -ge 3 ]; then
+                break;
+        else
+                echo "Restarting download because dislikes didn't show";
+        fi
 fi
 done
